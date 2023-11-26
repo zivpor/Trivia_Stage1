2023049b6ec2819b9f49ee2b6ef7b408f59bf0fe
Home <Home@LAPTOP-2E6V6ACM>
2023-11-26 15:15:25 +0200

    Signup Example

--- Trivia_Stage1/UI/Screen.cs
+++ Trivia_Stage1/UI/Screen.cs
@@ -16,7 +16,7 @@ namespace Trivia_Stage1.UI
         public virtual void Show()
         {
             Console.Clear();
-            Console.WriteLine($"\t\t\t\t\t{Title}");
+            Console.WriteLine($"{Title,65}");
         }
     }
 }
--- Trivia_Stage1/UI/TriviaScreensImp.cs
+++ Trivia_Stage1/UI/TriviaScreensImp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Trivia_Stage1.UI
@@ -22,9 +23,66 @@ namespace Trivia_Stage1.UI
         }
         public bool ShowSignup()
         {
-            Console.WriteLine("Not implemented yet! Press any key to continue...");
-            Console.ReadKey(true); 
-            return true;
+            //Logout user if anyone is logged in!
+            //A reference to the logged in user should be stored as a member variable
+            //in this class! Example:
+            //this.currentyPLayer == null
+
+            //Loop through inputs until a user/player is created or 
+            //user choose to go back to menu
+            char c = ' ';
+            while (c != 'B' && c != 'b' /*&& this.currentyPLayer == null*/)
+            {
+                //Clear screen
+                ClearScreenAndSetTitle("Signup");
+
+                Console.Write("Please Type your email: ");
+                string email = Console.ReadLine();
+                while (!IsEmailValid(email))
+                {
+                    Console.Write("Bad Email Format! Please try again:");
+                    email = Console.ReadLine();
+                }
+
+                Console.Write("Please Type your password: ");
+                string password = Console.ReadLine();
+                while (!IsPasswordValid(password))
+                {
+                    Console.Write("password must be at least 4 characters! Please try again: ");
+                    password = Console.ReadLine();
+                }
+
+                Console.Write("Please Type your Name: ");
+                string name = Console.ReadLine();
+                while (!IsNameValid(name))
+                {
+                    Console.Write("name must be at least 3 characters! Please try again: ");
+                    name = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("Connecting to Server...");
+                /* Create instance of Business Logic and call the signup method
+                 * For example:
+                try
+                {
+                    TriviaDBContext db = new TriviaDBContext();
+                    this.currentyPLayer = db.SignUp(email, password, name);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to signup! Email may already exist in DB!");
+                }
+                
+                */
+
+                //Provide a proper message for example:
+                Console.WriteLine("Press (B)ack to go back or any other key to signup again...");
+                //Get another input from user
+                c = Console.ReadKey(true).KeyChar;
+            }
+            //return true if signup suceeded!
+            return (false);
         }
 
         public void ShowAddQuestion()
@@ -47,6 +105,57 @@ namespace Trivia_Stage1.UI
         {
             Console.WriteLine("Not implemented yet! Press any key to continue...");
             Console.ReadKey(true);
+        }
+
+
+        //Private helper methods down here...
+        private void ClearScreenAndSetTitle(string title)
+        {
+            Console.Clear();
+            Console.WriteLine($"{title,65}");
+            Console.WriteLine();
+        }
+
+        private bool IsEmailValid(string emailAddress)
+        {
+            //regex is string based pattern to validate a text that follows a certain rules
+            // see https://learn.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+
+            var pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+            var regex = new Regex(pattern);
+            return regex.IsMatch(emailAddress);
+
+        //another option is using .net System.Net.Mail library which has an EmailAddress class that stores email
+        //we can use it to validate the structure of the email:
+       // https://learn.microsoft.com/en-us/dotnet/api/system.net.mail.mailaddress?view=net-7.0
+            /*
+             * try
+             * {
+             *     //try to create MailAddress objcect from the email address string
+             *      var email=new MailAddress(emailAddress);
+             *      //if success
+             *      return true;
+             * }
+             *      //if it throws a formatExcpetion then the string is not email format.
+             * catch (Exception ex)
+             * {
+             * return false;
+             * }
+             */
+
+        }
+
+
+
+        private bool IsPasswordValid(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 3;
+        }
+
+        private bool IsNameValid(string name)
+        {
+            return !string.IsNullOrEmpty(name) && name.Length >= 3;
         }
     }
 }
